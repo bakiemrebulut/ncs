a7ee59c935604e82f46fa5bfa0da76b337691c30
bakiemrebulut <bakiemrebulut@gmail.com>
2024-06-08 19:13:20 +0300

    first commit

---
+++ toolchains/cf2149caf2/opt/bin/Scripts/can_player.py
@@ -0,0 +1,11 @@
+#!c:\jenkins_workspace\workspace\est_ncs-toolchain-bundler_master\delete_me\temp\install_python\33c9f772-0de3-4b14-a23a-d77b9159e5bb\python.exe
+
+"""
+See :mod:`can.player`.
+"""
+
+from can.player import main
+
+
+if __name__ == "__main__":
+    main()
